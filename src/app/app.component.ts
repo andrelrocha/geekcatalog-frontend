09db72376ac7c048235b69ff6503a80ee9660bd5
andrelrocha <andrerocha0911@gmail.com>
2024-03-27 11:01:33 -0300

    feat: cabecalho da aplicaçao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppFooterComponent],
+  imports: [RouterOutlet, AppFooterComponent, AppHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
