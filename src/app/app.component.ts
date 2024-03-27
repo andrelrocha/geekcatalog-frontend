de08ccd0270f669762af1adc1a83d082e9dd9b06
andrelrocha <andrerocha0911@gmail.com>
2024-03-27 10:21:03 -0300

    feat footer da aplicaçao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
